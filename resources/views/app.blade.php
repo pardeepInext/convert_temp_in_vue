766e05b0363d2ef42a80c4ad06793ff24c5895bb
inext51 <pardeepk.inext@gmail.com>
2021-08-09 16:22:32 +0530

    add http

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicon/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicon/favicon-16x16.png') }}">
     <link rel="manifest" href="{{ asset('assets/favicon/site.webmanifest') }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <title>Aler</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
